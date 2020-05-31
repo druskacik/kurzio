635403b1b93f57c253bbb3f3cc980522a9017756
RobertDruska <robert.druska@gmail.com>
2020-05-31 22:33:47 +0200

    nodemailer - transport set to secure

--- src/services/mailer/index.js
+++ src/services/mailer/index.js
@@ -6,7 +6,7 @@ const environment = process.env.NODE_ENV || 'development';
 const transporter = mailer.createTransport({
 	host: process.env.MAIL_HOST,
 	port: process.env.MAIL_PORT || '465',
-	secure: process.env.MAIL_PORT === '465',
+	secure: 'true',
 	auth: {
 		user: process.env.MAIL_USER,
 		pass: process.env.MAIL_PASS,
