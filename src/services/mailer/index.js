0396a7cde2b0740505f9d0a77538f43dfbec7ddb
RobertDruska <robert.druska@gmail.com>
2020-05-31 22:42:27 +0200

    mail port

--- src/services/mailer/index.js
+++ src/services/mailer/index.js
@@ -5,8 +5,8 @@ const environment = process.env.NODE_ENV || 'development';
 
 const transporter = mailer.createTransport({
 	host: process.env.MAIL_HOST,
-	port: process.env.MAIL_PORT || '465',
-	secure: 'true',
+	port: process.env.MAIL_PORT,
+	secure: process.env.MAIL_PORT === '465',
 	auth: {
 		user: process.env.MAIL_USER,
 		pass: process.env.MAIL_PASS,
