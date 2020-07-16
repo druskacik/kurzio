51bc0ea2549b744a497d2f76cbdbd25614de8560
RobertDruska <robert.druska@gmail.com>
2020-07-16 19:21:46 +0200

    other sports

--- src/utils/get-slovak-name.js
+++ src/utils/get-slovak-name.js
@@ -4,6 +4,23 @@ const getSlovakName = (name) => {
     'ice hockey': 'Hokej',
     tennis: 'Tenis',
     basketball: 'Basketbal',
+    baseball: 'Baseball',
+    golf: 'Golf',
+    cycling: 'Cyklistika',
+    'american football': 'Americký futbal',
+    'australian football': 'Austrálsky futbal',
+    'e-sports': 'E-športy',
+    motosport: 'Motošport',
+    'fight sports': 'Bojové športy',
+    'horse racing': 'Dostihy',
+    box: 'Box',
+    chess: 'Šach',
+    society: 'Spoločenské  stávky',
+    pingpong: 'Stolný tenis',
+    darts: 'Šípky',
+    rugby: 'Rugby',
+    snooker: 'Snooker',
+    volleyball: 'Volejbal'
   }
 
   return names[name];
