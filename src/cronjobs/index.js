223be06ba7ffdc91a7261c425ab39a55126a834c
RobertDruska <robert.druska@gmail.com>
2020-07-17 23:09:26 +0200

    clean database once a day

--- src/cronjobs/index.js
+++ src/cronjobs/index.js
@@ -1,5 +1,7 @@
 const fetchOddsJob = require('./fetch-political-odds');
 const fetchCompetitionsJob = require('./competitions-and-matches');
+const cleanDBJob = require('./clean-database');
 
 fetchOddsJob.start();
 fetchCompetitionsJob.start();
+cleanDBJob.start();
