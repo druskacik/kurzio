b9c72e63bfa17f633ee89877a5b6953a61b557e5
RobertDruska <robert.druska@gmail.com>
2020-08-28 21:53:41 +0200

    db change

--- knexfile.js
+++ knexfile.js
@@ -29,16 +29,28 @@ module.exports = {
     }
   },
 
+  // for heroku DB
+  // production: {
+  //   client: 'postgresql',
+  //   connection: process.env.DATABASE_URL,
+  //   pool: {
+  //     min: 2,
+  //     max: 10
+  //   },
+  //   migrations: {
+  //     directory: __dirname + '/migrations',
+  //   }
+  // }
+
   production: {
     client: 'postgresql',
-    connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      directory: __dirname + '/migrations',
+    connection: {
+      host     : process.env.DB_HOST,
+      user     : process.env.DB_USER,
+      password : process.env.DB_PASS,
+      database : process.env.DB_NAME,
+      port     : process.env.DB_PORT,
     }
-  }
+  },
 
 };
