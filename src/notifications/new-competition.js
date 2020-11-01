bd40400c81b84cb487cabb02c5d7552a44d76f88
RobertDruska <robert.druska@gmail.com>
2020-11-01 19:11:15 +0100

    juros request

--- src/notifications/new-competition.js
+++ src/notifications/new-competition.js
@@ -5,10 +5,14 @@ const getSlovakName = require('../utils/get-slovak-name');
 
 const newCompetitionNotification = async (sport, name) => {
   for (let email of sport.emails) {
-    await sendNotificationEmail(email, {
-      sportName: getSlovakName(sport.sport_name),
-      competition: name,
-    });
+    if (email.address === 'druskajuraj@gmail.com' && name.substring(0, 3) === 'ITF') {
+      console.log('Not sending email because Juro does not really bet on ITF competitions.');
+    } else {
+      await sendNotificationEmail(email, {
+        sportName: getSlovakName(sport.sport_name),
+        competition: name,
+      });
+    }
   }
 }
 
