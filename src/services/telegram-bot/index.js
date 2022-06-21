f743971665fb588a4380a30a7b4257e8191f920e
RobertDruska <robert.druska@gmail.com>
2022-06-21 13:28:15 +0200

    telegram bot functions

--- src/services/telegram-bot/index.js
+++ src/services/telegram-bot/index.js
@@ -1,22 +1,30 @@
 const sendStartMessage = require('./messages/start');
 const sendListMessage = require('./messages/list');
+const sendSportsMessage = require('./messages/sports');
 const sendTrackMessage = require('./messages/track');
 const sendUntrackMessage = require('./messages/untrack');
+const sendTrackSportMessage = require('./messages/tracksport');
+const sendUntrackSportMessage = require('./messages/untracksport');
 const sendSettingsMessage = require('./messages/settings');
 const sendHelpMessage = require('./messages/help');
 const sendStatusMessage = require('./messages/status');
 const sendSupportMessage = require('./messages/support');
 
 const sendNewMatchesNotification = require('./notifications/new-matches');
+const sendNewCompetitionNotification = require('./notifications/new-competition');
 
 module.exports = {
     sendStartMessage,
     sendListMessage,
+    sendSportsMessage,
     sendTrackMessage,
     sendUntrackMessage,
+    sendTrackSportMessage,
+    sendUntrackSportMessage,
     sendSettingsMessage,
     sendHelpMessage,
     sendStatusMessage,
     sendSupportMessage,
     sendNewMatchesNotification,
+    sendNewCompetitionNotification,
 }
