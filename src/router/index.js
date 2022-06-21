33666a4bf9bb2540d23318ef262aa33354deba5a
RobertDruska <robert.druska@gmail.com>
2022-06-21 14:12:43 +0200

    maintenance mode setup + obsolete files deleted

--- src/router/index.js
+++ src/router/index.js
@@ -2,37 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
-// const sportsAndCompetitions = require('./routes/sport-and-competitions');
-// const subscription = require('./routes/subscription');
 const telegramBotWebhook = require('./telegram-webhook');
 
 const app = express();
 
 app.use(cors());
 
-// enforce HTTPS
-const requireHTTPS = (req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === "production") {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-
-app.use(requireHTTPS);
-
 // starts all cronjobs
-require('../cronjobs');
+if (!(process.env.MAINTENANCE_MODE == 1)) {
+    require('../cronjobs');
+} else {
+    console.log('Maintenance mode on, cronjobs are not being run !');
+}
 
 app.use(bodyParser.json());
 
-// app.use(express.static(path.join(__dirname, '../pages')));
+app.get('/', (req, res) => {
+    res.status(200).end('ok');
+});
 
-// serve vue build
-app.use(express.static('./dist'));
-
-// app.use('/api/get-sports', sportsAndCompetitions);
-// app.use('/api/subscription', subscription);
 app.use(`/api/telegram${process.env.TELEGRAM_BOT_TOKEN}`, telegramBotWebhook);
 
 const listen = (port) => {
