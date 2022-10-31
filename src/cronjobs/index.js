330523b944f61058b1c5bf66f08d5773c87e54c1
Robert Druska <robert.druska@gmail.com>
2022-10-31 10:32:02 +0100

    disable odds fetching

--- src/cronjobs/index.js
+++ src/cronjobs/index.js
@@ -1,5 +1,5 @@
 require('./fetch-sports-and-competitions');
 require('./fetch-matches');
-require('./fetch-odd-types');
-require('./fetch-odds');
+// require('./fetch-odd-types');
+// require('./fetch-odds');
 require('./toggle-admin-notification-status');
